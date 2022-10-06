0b1ed11c748c198e048cf4a762967c17f5d0a740
Isabella <isabellarojas@uvic.ca>
2022-10-06 16:01:10 -0700

    checking

---
+++ MissionariesandCannibalsState.java
@@ -0,0 +1,43 @@
+public class MissionariesandCannibalsState {
+  
+    int[] startSide;
+    int[] endSide;
+    boolean boatStartSide;
+    
+    public MissionariesandCannibalsState(int[] startSide, int[] endSide, boolean boatStartSide) { 
+    	this.startSide = startSide; 
+        this.endSide = endSide;
+        this.boatStartSide = boatStartSide;
+    }
+    
+    // Number of missionaries will be index 0, number of cannibals will be index 1
+    public MissionariesandCannibalsState(MissionariesandCannibalsState state) {
+        startSide = new int[2];
+        endSide = new int[2];
+    
+        for(int i=0; i<2; i++) {
+            startSide[i] = state.startSide[i];
+            endSide[i] = state.endSide[i];
+        }
+        boatStartSide = state.boatStartSide;
+    }
+    
+    public boolean equals(Object o) {
+        return java.util.Arrays.equals( startSide, ((MissionariesandCannibalsState) o).startSide ) &&
+               java.util.Arrays.equals( endSide, ((MissionariesandCannibalsState) o).endSide ) &&
+               (boatStartSide == ((MissionariesandCannibalsState) o).boatStartSide);
+    }
+    
+    public int hashCode() {
+        return (java.util.Arrays.hashCode( startSide ) + java.util.Arrays.hashCode( endSide ));
+    }    
+    
+    public String toString() {
+        if (boatStartSide) {
+            return ("B " + java.util.Arrays.toString( startSide ) + " " + java.util.Arrays.toString( endSide ) + " |");
+        } else {
+            return (java.util.Arrays.toString( startSide ) + " B " + java.util.Arrays.toString( endSide ) + " |");
+        }
+    	
+    }
+}
